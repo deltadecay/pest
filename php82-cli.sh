89bb3fe743b7f77ed60b565f62d9fa196220e34f
deltadecay <74830817+deltadecay@users.noreply.github.com>
2024-02-05 16:25:33 +0100

    Updated php82 docker script

--- php82-cli.sh
+++ php82-cli.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker run -it --rm --name php82-cli -v "$PWD":/usr/src/myapp -w /usr/src/myapp php:8.2.12-cli-alpine3.18 php $@
+docker run -it --rm --name php82-cli -v "$PWD":/usr/src/myapp -w /usr/src/myapp php:8.2.15-cli-alpine3.19 php $@
 
