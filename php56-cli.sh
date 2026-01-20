a1408d780ee6f6f751fae48ac11c2caa7b0f9055
deltadecay <74830817+deltadecay@users.noreply.github.com>
2026-01-20 14:52:59 +0100

    Removed the php scripts

--- php56-cli.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker run -it --rm --name php56-cli -v "$PWD":/usr/src/myapp -w /usr/src/myapp php:5.6.40-cli-alpine php $@
-
--- php82-cli.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker run -it --rm --name php82-cli -v "$PWD":/usr/src/myapp -w /usr/src/myapp php:8.2.15-cli-alpine3.19 php $@
-
